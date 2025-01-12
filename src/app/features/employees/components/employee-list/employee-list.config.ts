740b7e1e08ca70278180360218b8bd4bfec368b0
Buffden <buffdenslays@gmail.com>
2025-01-12 05:18:45 -0600

    Feature/employee-1/implement-employee-list-table
    rendered the employee-list table successfully

---
+++ src/app/features/employees/components/employee-list/employee-list.config.ts
@@ -0,0 +1,17 @@
+import { TableConfig } from "../../../../shared/models/table";
+
+export const employeeListConfig: TableConfig = {
+    columns: [
+        { key: 'address', header: 'address', sortable: true, type: 'string' },
+        { key: 'name', header: 'Employee Name', sortable: true, type: 'string' },
+        { key: 'department', header: 'Department', sortable: false, type: 'string' },
+        { key: 'designation', header: 'Designation', sortable: true, type: 'number' },
+        { key: 'email', header: 'Salary', sortable: true, type: 'string' },
+        { key: 'phone', header: 'Phone', sortable: true, type: 'string' },
+        { key: 'manager', header: 'Manager', sortable: true, type: 'string' },
+        { key: 'id', header: 'ID', sortable: true, type: 'number' },
+        { key: 'joiningDate', header: 'Joining Date', sortable: true, type: 'date' },
+    ],
+    pageSize: 10,
+    pageSizeOptions: [5, 10, 25, 50, 100],
+};
