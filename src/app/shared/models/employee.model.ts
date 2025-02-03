848dec48e34b92e8522868a6f4d5164a329c55bc
Buffden <buffdenslays@gmail.com>
2025-02-03 17:06:46 -0600

    Feature/generic-overlay/employee-add-form
    employee add form working fine now

--- src/app/shared/models/employee.model.ts
+++ src/app/shared/models/employee.model.ts
@@ -14,3 +14,22 @@ export interface EmployeeFormField {
     placeholder: string;
     errorMessage: string;
 }
+
+export interface EmployeeRequest {
+    name: string;
+    designation: string;
+    salary: number;
+    department: DepartmentID;
+    manager: ManagerID;
+    phone: string;
+    address: string;
+    email: string;
+}
+
+export interface ManagerID {
+    id: Employee | ManagerID;
+}
+
+export interface DepartmentID {
+    id: Department | DepartmentID;
+}
